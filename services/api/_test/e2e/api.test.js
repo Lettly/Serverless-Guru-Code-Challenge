92b94fc2775de91fa6c6575c605968ff2bc59454
Marco Giuseppini <giuseppini.marco@live.it>
2023-01-12 16:26:54 +0100

    Added read pending order test

--- services/api/_test/e2e/api.test.js
+++ services/api/_test/e2e/api.test.js
@@ -81,6 +81,30 @@ describe("A call to the api can", () => {
         expect(dbEntry).toEqual(res.data.order)
     })
 
+    test('get the orders pending in a asc order', async () => {
+        await createDbEntry("marco", "2021-02-01T00:00:00.000Z")
+        await createDbEntry("marco", "2021-02-02T00:00:00.000Z")
+
+        const res = await axios.get(`${process.env.HttpApiUrl}/orders`).catch(
+            (err) => {
+                console.log(err.response.data);
+                throw err
+            }
+        )
+
+        expect(res.status).toBe(200)
+        expect(res.data.orders).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(
+                    {
+                        customerId: expect.any(String),
+                        date: expect.any(String),
+                    }
+                )
+            ])
+        )
+    })
+
     test('edit an order', async () => {
         const order = await createDbEntry("marco", "2021-01-02T00:00:00.000Z")
         order.status = "delivered"
