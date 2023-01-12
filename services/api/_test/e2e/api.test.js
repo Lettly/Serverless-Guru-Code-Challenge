a8ace7c5606d9c27ed46d3b3c22ea7f4ac0e89b8
Marco Giuseppini <giuseppini.marco@live.it>
2023-01-12 14:18:41 +0100

    Added e2e test for CRUD

--- services/api/_test/e2e/api.test.js
+++ services/api/_test/e2e/api.test.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 import axios from 'axios'
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 const client = new DynamoDBClient({ region: process.env.Region });
 const doc = DynamoDBDocumentClient.from(client)
@@ -19,7 +19,26 @@ async function checkDbEntry(customerId, date) {
     return order?.Item;
 }
 
+async function createDbEntry(customerId, date) {
+    const order = {
+        customerId,
+        date,
+        item: 1,
+        quantity: 1,
+        status: "pending",
+    }
+
+    const command = new PutCommand({
+        TableName: process.env.OrdersTable,
+        Item: order,
+    });
+
+    await doc.send(command);
+    return order;
+}
+
 describe("A call to the api can", () => {
+
     test('create an order', async () => {
         const order = {
             "customerId": "marco",
@@ -43,5 +62,60 @@ describe("A call to the api can", () => {
 
         const dbEntry = await checkDbEntry(res.data.order.customerId, res.data.order.date)
         expect(dbEntry).toEqual(res.data.order)
+    })
+
+    test('get an order', async () => {
+        const order = await createDbEntry("marco", "2021-01-01T00:00:00.000Z")
+
+        const res = await axios.get(`${process.env.HttpApiUrl}/orders/${order.customerId}/${order.date}`).catch(
+            (err) => {
+                console.log(err.response.data);
+                throw err
+            }
+        )
+
+        expect(res.status).toBe(200)
+        expect(res.data.order).toEqual(order)
+
+        const dbEntry = await checkDbEntry(res.data.order.customerId, res.data.order.date)
+        expect(dbEntry).toEqual(res.data.order)
+    })
+
+    test('edit an order', async () => {
+        const order = await createDbEntry("marco", "2021-01-02T00:00:00.000Z")
+        order.status = "delivered"
+
+        const res = await axios.put(`${process.env.HttpApiUrl}/orders`, order).catch(
+            (err) => {
+                console.log(err.response.data);
+                throw err
+            }
+        )
+
+        expect(res.status).toBe(200)
+        expect(res.data.order).toEqual(order)
+
+        const dbEntry = await checkDbEntry(res.data.order.customerId, res.data.order.date)
+        expect(dbEntry).toEqual(res.data.order)
+    })
+
+    test('delete an order', async () => {
+        const order = await createDbEntry("marco", "2021-01-03T00:00:00.000Z")
+
+        const res = await axios.delete(`${process.env.HttpApiUrl}/orders/${order.customerId}/${order.date}`).catch(
+            (err) => {
+                console.log(err.response.data);
+                throw err
+            }
+        )
+
+        expect(res.status).toBe(200)
+        expect(res.data.order).toEqual({
+            customerId: order.customerId,
+            date: order.date,
+        })
+
+        const dbEntry = await checkDbEntry(res.data.order.customerId, res.data.order.date)
+        expect(dbEntry).toEqual(undefined)
     })
 })
